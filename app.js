b5667efaf4eecd4fd9ed2f75048949a0e2046e33
Deb <30626327+dsharp4u@users.noreply.github.com>
2021-09-12 21:59:36 -0400

    Add files via upload
    added the app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElelemt.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "796615aa66205a1254376c90ff6d0826";
+let aoiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metic`;
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric';
+
+axios.get(aoiUrl).then(displayTemperature);
