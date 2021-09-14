fe1f5a1d69183a2f9a4e7feac213a9975bde7806
Deb <30626327+dsharp4u@users.noreply.github.com>
2021-09-13 21:13:21 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}: ${minute}`;
+  //return "Friday 05:00";
+}
+
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = response.data.name;
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = "Friday 05:00";
+  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "796615aa66205a1254376c90ff6d0826";
-let aoiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metic`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=796615aa66205a1254376c90ff6d0826&units=metric`;
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=796615aa66205a1254376c90ff6d0826&units-metric`;
 //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric';
 
-axios.get(aoiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
